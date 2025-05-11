2612a9d3749198c8510858771d03acd9893ab2fc
Olyco <72895415+Olyco@users.noreply.github.com>
2025-05-12 02:28:12 +0300

    Create ETTh1_48for12_5.py

---
+++ config/reproduce_conf/MLP/ETTh1_48for12_5.py
@@ -0,0 +1,17 @@
+exp_conf = dict(
+    model_name="KAN",
+    dataset_name='ETTh1',
+
+    hist_len=48,
+    pred_len=12,
+
+    max_epochs=5,
+
+    width=[48 * 7, 5, 12 * 7],
+    grid=3,
+    k=3,
+
+    lr=0.01,
+
+    save_root="drive/MyDrive/VKR/Results/save"
+)
