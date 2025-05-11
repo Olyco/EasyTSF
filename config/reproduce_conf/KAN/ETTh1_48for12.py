3c845633686099f3e765832f2d956424aa640a68
Olyco <72895415+Olyco@users.noreply.github.com>
2025-05-11 19:46:23 +0300

    Update and rename ETTh1_96for48.py to ETTh1_48for12.py

--- config/reproduce_conf/KAN/ETTh1_48for12.py
+++ config/reproduce_conf/KAN/ETTh1_48for12.py
@@ -2,12 +2,12 @@ exp_conf = dict(
     model_name="KAN",
     dataset_name='ETTh1',
 
-    hist_len=96,
-    pred_len=48,
+    hist_len=48,
+    pred_len=12,
 
     max_epochs=3,
 
-    width=[96 * 7, 50, 50 , 48 * 7],
+    width=[48 * 7, 20 , 12 * 7],
     grid=3,
     k=3,
 
