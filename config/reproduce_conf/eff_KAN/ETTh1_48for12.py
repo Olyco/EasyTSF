25a7c116a2545018c4a1cc20113112f1a3939934
Olyco <72895415+Olyco@users.noreply.github.com>
2025-05-12 07:35:16 +0300

    Create ETTh1_48for12.py for eff_KAN

---
+++ config/reproduce_conf/eff_KAN/ETTh1_48for12.py
@@ -0,0 +1,17 @@
+exp_conf = dict(
+    model_name="eff_KAN",
+    dataset_name='ETTh1',
+
+    hist_len=48,
+    pred_len=12,
+
+    max_epochs=5,
+
+    width=[48 * 7, 20 , 12 * 7],
+    grid=3,
+    k=3,
+
+    lr=0.01,
+
+    save_root="drive/MyDrive/VKR/Results/save"
+)
